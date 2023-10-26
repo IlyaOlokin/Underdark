60f25736ba4c87cedf2f3837172c20c872dc8a4a
IlyaOlokin <i.olokin@mail.ru>
2023-10-26 17:05:21 +0500

    drop

--- Underdark/Assets/Scripts/SO/Items/DroppedItem.cs
+++ Underdark/Assets/Scripts/SO/Items/DroppedItem.cs
@@ -2,31 +2,56 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DroppedItem : MonoBehaviour
 {
     [SerializeField] private Item containedItem;
     [SerializeField] private int itemAmount = 1;
+    [SerializeField] private float speed;
+    [SerializeField] private float timeToIntractable;
+    private bool picked;
+    private Transform target;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Collider2D collider;
 
     private void Awake()
     {
-        SetDroppedItem(containedItem, itemAmount);
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        collider = GetComponent<CircleCollider2D>();
+        //SetDroppedItem(containedItem, itemAmount);
+    }
+
+    private void Update()
+    {
+        timeToIntractable -= Time.deltaTime;
+        if (timeToIntractable <= 0) collider.enabled = true;
+        if (!picked) return;
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.1f) Destroy(gameObject);
+        else transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 
     public void SetDroppedItem(Item item, int amount)
     {
         containedItem = item;
         itemAmount = amount;
-        GetComponent<SpriteRenderer>().sprite = item.Sprite;
+        sr.sprite = item.Sprite;
+        rb.AddForce(new Vector2(Random.Range(-1f,1f), Random.Range(-1f, 1f)) * 3, ForceMode2D.Impulse);
+        
+        collider.enabled = false;
     }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent(out IPickUper pickUper))
+        if (!picked && other.TryGetComponent(out IPickUper pickUper))
         {
             if (pickUper.TryPickUpItem(containedItem, itemAmount))
             {
-                Destroy(gameObject);
+                picked = true;
+                target = other.transform;
             }
         }
     }
