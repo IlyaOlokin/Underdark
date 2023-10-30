bb33afe6f561cbb60b49d381573aa5cd8c1ed2b3
IlyaOlokin <i.olokin@mail.ru>
2023-10-30 20:27:48 +0500

    puncture visual

--- Underdark/Assets/Scripts/ActiveAblities/AccuratePuncture.cs
+++ Underdark/Assets/Scripts/ActiveAblities/AccuratePuncture.cs
@@ -1,7 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class AccuratePuncture : ActiveAblity, IAttacker
 {
+    [Header("Visual")]
+    [SerializeField] private Transform visual;
+    [SerializeField] private float visualDuration;
+    [SerializeField] private float endXScale;
+    [SerializeField] private float startYScale;
+    
     public override void Execute(Unit caster)
     {
         this.caster = caster;
@@ -12,9 +19,25 @@ public class AccuratePuncture : ActiveAblity, IAttacker
         if (target == null) return;
         Attack(target.GetComponent<IDamageable>());
         
-        // Instantiate visual
+        StartCoroutine(StartVisual(target.transform));
     }
-
+    IEnumerator StartVisual(Transform target)
+    {
+        visual.gameObject.SetActive(true);
+        visual.position = target.position;
+        visual.localScale = new Vector3(0, startYScale);
+        float scaleXSpeed =  endXScale / visualDuration;
+        float scaleYSpeed = -visual.localScale.y / visualDuration;
+        visual.eulerAngles = new Vector3(0, 0, Random.Range(0, 360));
+        
+        while (visualDuration > 0)
+        {
+            visualDuration -= Time.deltaTime;
+            visual.localScale += new Vector3(scaleXSpeed * Time.deltaTime, scaleYSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+    
     
     public void Attack()
     {
