5249ae1c81ebf75074bcf25dee46a619e1cbd049
IlyaOlokin <i.olokin@mail.ru>
2023-11-04 20:24:34 +0500

    circular swing

---
+++ Underdark/Assets/Scripts/ActiveAblities/CircularSwing.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularSwing : ActiveAbility, IAttacker
+{
+    public Transform Transform => transform;
+    [Header("Visual")]
+    [SerializeField] private BaseAttackVisual visual;
+    
+    
+    public override void Execute(Unit caster)
+    {
+        this.caster = caster;
+        damage = Mathf.Min(caster.Stats.Strength * statMultiplier, maxValue);
+        
+        Attack();
+        
+        var attackDirAngle = Vector3.Angle(Vector3.right, caster.GetAttackDirection());
+        visual.Swing(attackDirAngle, attackAngle, attackDistance);
+    }
+    
+    public void Attack()
+    {
+        var targets = FindAllTargets(caster);
+
+        foreach (var target in targets)
+        {
+            if (target.GetComponent<IDamageable>().TakeDamage(caster, damage))
+            {
+                foreach (var debuffInfo in debuffInfos)
+                {
+                    debuffInfo.Execute(caster, target.GetComponent<Unit>(), caster);
+                }
+            }
+        }
+    }
+
+    public void Attack(IDamageable unit)
+    {
+        throw new System.NotImplementedException();
+    }
+}
