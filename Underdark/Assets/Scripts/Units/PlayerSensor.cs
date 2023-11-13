c5e9ce452bc7c5e56fe162d2a1577247794dcfa4
IlyaOlokin <i.olokin@mail.ru>
2023-11-13 17:07:53 +0500

    player sensor

--- Underdark/Assets/Scripts/Units/PlayerSensor.cs
+++ Underdark/Assets/Scripts/Units/PlayerSensor.cs
@@ -6,14 +6,29 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class PlayerSensor : MonoBehaviour
 {
+    [SerializeField] private LayerMask layerMask;
     public event Action<Transform> OnPlayerEnter;
     public event Action<Vector3> OnPlayerExit;
+    private bool needRayCast;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Player player))
         {
-            OnPlayerEnter?.Invoke(player.transform);
+            needRayCast = true;
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (needRayCast && other.TryGetComponent(out Player player))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, Mathf.Infinity, layerMask);
+            if (hit.collider != null && hit.transform.CompareTag("Player"))
+            {
+                OnPlayerEnter?.Invoke(player.transform);
+                needRayCast = false;
+            }
         }
     }
 
