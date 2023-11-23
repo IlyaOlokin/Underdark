4ae3d9e1eb380b6bbc4c30d6e3cef4daf0b13f05
IlyaOlokin <i.olokin@mail.ru>
2023-11-23 09:23:48 +0500

    item storage

---
+++ Underdark/Assets/Scripts/SO/ItemsStorageSO.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ItemsStorage", fileName = "ItemStorage")]
+public class ItemsStorageSO : ScriptableObject
+{
+    [field: SerializeField] public List<Item> Items { get; private set; }
+
+    public void LoadItems()
+    {
+        Items.Clear();
+        var items = Resources.LoadAll<Item>("Items");
+
+        foreach (var item in items)
+        {
+            Items.Add(item);
+        }
+    }
+
+    public void Clear()
+    {
+        Items.Clear();
+    }
+
+    public Item GetItemById(string id)
+    {
+        foreach (var item in Items)
+        {
+            if (item.ID == id)
+                return item;
+        }
+        Debug.Log($"No item with ID {id} was found.");
+
+        return null;
+    }
+}
