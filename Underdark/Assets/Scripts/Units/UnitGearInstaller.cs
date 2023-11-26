b832768dd029ca3e8a08e963b7b9719d1f4a6c94
IlyaOlokin <i.olokin@mail.ru>
2023-11-26 09:24:41 +0500

    gear installer

---
+++ Underdark/Assets/Scripts/Units/UnitGearInstaller.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class UnitGearInstaller : MonoBehaviour
+{
+    [SerializeField] private Item head;
+    [SerializeField] private Item body;
+    [SerializeField] private Item legs;
+    [SerializeField] private Item shield;
+    [SerializeField] private Item weapon;
+    
+    [SerializeField] private List<Item> executableItems;
+    
+    [SerializeField] private List<Item> activeAbilities;
+
+    private void Start()
+    {
+        Unit unit = GetComponent<Unit>();
+        
+        unit.Inventory.Equipment.Head.SetItem(head);
+        unit.Inventory.Equipment.Body.SetItem(body);
+        unit.Inventory.Equipment.Legs.SetItem(legs);
+        unit.Inventory.Equipment.Shield.SetItem(shield);
+        unit.Inventory.Equipment.Weapon.SetItem(weapon);
+
+        var loopsExe = Mathf.Min(unit.Inventory.ExecutableSlots.Count, executableItems.Count);
+
+        for (int i = 0; i < loopsExe; i++)
+            unit.Inventory.ExecutableSlots[i].SetItem(executableItems[i]);
+
+        var loopsAA = Mathf.Min(unit.Inventory.EquippedActiveAbilitySlots.Count, activeAbilities.Count);
+        for (int i = 0; i < loopsAA; i++)
+            unit.Inventory.EquippedActiveAbilitySlots[i].SetItem(activeAbilities[i]);
+        
+        Destroy(this);
+    }
+}
