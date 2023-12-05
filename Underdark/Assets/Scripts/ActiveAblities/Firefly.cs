68afe81dd4b89d7405cb915b51805f2c5e3fd0e5
IlyaOlokin <i.olokin@mail.ru>
2023-12-05 20:46:51 +0500

    firefly

---
+++ Underdark/Assets/Scripts/ActiveAblities/Firefly.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+public class Firefly : ActiveAbility
+{
+    private Vector3 currentTargetDir;
+    
+    [SerializeField] private float distanceFromCaster;
+    [SerializeField]private float speed;
+    
+    [SerializeField] private float changePositionDelay;
+    private float changePositionTimer;
+
+    private void Update()
+    {
+        changePositionTimer -= Time.deltaTime;
+        if (changePositionTimer <= 0)
+        {
+            SetNewTargetPosition();
+            changePositionTimer = changePositionDelay;
+        }
+
+        transform.position = Vector3.Lerp(transform.position,
+            caster.transform.position + (currentTargetDir * distanceFromCaster), speed * Time.deltaTime);
+    }
+
+    private void SetNewTargetPosition()
+    {
+        currentTargetDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
