69f5d3e6a020050a8885062e900a76aa51764771
IlyaOlokin <i.olokin@mail.ru>
2023-12-18 12:59:10 +0500

    death window fix

--- Underdark/Assets/Scripts/UI/DeathWindow.cs
+++ Underdark/Assets/Scripts/UI/DeathWindow.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -11,6 +12,13 @@ public class DeathWindow : MonoBehaviour
 
     [SerializeField] private GameObject content;
     [SerializeField] private Button respawnButton;
+
+    [Header("Visual")] 
+    [SerializeField] private float appearDuration;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Image blackout;
+    [SerializeField] private Image buttonImage;
+        
     
     [Inject]
     private void Construct(Player player)
@@ -21,19 +29,54 @@ public class DeathWindow : MonoBehaviour
 
     private void OnEnable()
     {
-        player.OnUnitDeath += ActivateContent;
+        player.OnUnitDeath += StartActivateContent;
     }
     
     private void OnDisable()
     {
-        player.OnUnitDeath -= ActivateContent;
+        player.OnUnitDeath -= StartActivateContent;
     }
 
-    private void ActivateContent()
+    private void StartActivateContent()
+    {
+        StartCoroutine(ActivateContent());
+    }
+
+    IEnumerator ActivateContent()
     {
         transform.SetAsLastSibling();
         content.SetActive(true);
+
+        float timer = 0f; 
+
+        while (timer < appearDuration)
+        {
+            timer += Time.deltaTime; 
+            
+            float progress = Mathf.Clamp01(timer / appearDuration);
+            
+            ChangeColorAlpha(text, progress);
+            ChangeColorAlpha(blackout, progress);
+            ChangeColorAlpha(buttonImage, progress);
+
+            yield return null;
+        }
     }
+
+    private void ChangeColorAlpha(Image image, float a)
+    {
+        Color newColor = image.color;
+        newColor.a = a;
+        image.color = newColor;
+    }
+    
+    private void ChangeColorAlpha(TextMeshProUGUI image, float a)
+    {
+        Color newColor = image.color;
+        newColor.a = a;
+        image.color = newColor;
+    }
+    
 
     private void LoadHub()
     {
