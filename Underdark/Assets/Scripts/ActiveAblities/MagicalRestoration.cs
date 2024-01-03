a63a9a516521b9aa8381cfb7752fbba26b9d3ae7
IlyaOlokin <i.olokin@mail.ru>
2024-01-03 16:28:50 +0500

    ability description

--- Underdark/Assets/Scripts/ActiveAblities/MagicalRestoration.cs
+++ Underdark/Assets/Scripts/ActiveAblities/MagicalRestoration.cs
@@ -20,7 +20,7 @@ public class MagicalRestoration : ActiveAbility
     
     public override string[] ToString()
     {
-        var res = new string[3];
+        var res = new string[6];
         res[0] = description;
         res[1] = $"Heal: {statMultiplier} * {UnitStats.GetStatString(baseStat)} (max: {maxValue})";
         if (ManaCost != 0) res[2] = $"Mana: {ManaCost}";
