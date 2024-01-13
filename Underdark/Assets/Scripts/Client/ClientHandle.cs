cef3faf91d6cd0baa1c014f1d245dd635a9f5d58
IlyaOlokin <i.olokin@mail.ru>
2024-01-13 14:42:36 +0500

    tests

---
+++ Underdark/Assets/Scripts/Client/ClientHandle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using TMPro;
+using UnityEngine;
+
+public class ClientHandle : MonoBehaviour
+{
+    private static TextMeshProUGUI logStatic;
+    [SerializeField] private TextMeshProUGUI log;
+    
+    private void Awake()
+    {
+        logStatic = log;
+    }
+
+    public static void Welcome(Packet _packet)
+    {
+        string _msg = _packet.ReadString();
+        int _myId = _packet.ReadInt();
+
+        logStatic.text += $"Message from server: {_msg}\n";
+        Debug.Log($"Message from server: {_msg}");
+        Client.instance.myId = _myId;
+        ClientSend.WelcomeReceived();
+
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+
+    public static void UDPTest(Packet _packet)
+    {
+        string _msg = _packet.ReadString();
+
+        logStatic.text += $"Received packet via UDP. Contains message: {_msg}\n";
+        Debug.Log($"Received packet via UDP. Contains message: {_msg}");
+        ClientSend.UDPTestReceived();
+    }
+}
---
+++ Underdark/Assets/Scripts/Client/IPManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class IPManager
+{
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+    
+    public static string GetGlobalIPAddress()
+    {
+        var url = "https://api.ipify.org/";
+
+        WebRequest request = WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        Stream dataStream = response.GetResponseStream();
+
+        using StreamReader reader = new StreamReader(dataStream);
+
+        var ip = reader.ReadToEnd();
+        reader.Close();
+
+        return ip;
+    }
+}
