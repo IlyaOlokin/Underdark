42782ddfbb5f109206539402b70b72d2a4cbf35a
IlyaOlokin <i.olokin@mail.ru>
2024-02-11 15:41:21 +0500

    fix

--- Underdark/Assets/Scripts/ActiveAblities/ActiveAbilityProperty.cs
+++ Underdark/Assets/Scripts/ActiveAblities/ActiveAbilityProperty.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class ActiveAbilityProperty<T>
+public class ActiveAbilityProperty<T> where T : new()
 {
     [SerializeField] private List<T> values;
 
     public T GetValue(int level)
     {
         if (level > 0) level -= 1;
+
+        if (values.Count == 0) return new T();
         
         return values.Count > level ? values[level] : values[^1];
     }
