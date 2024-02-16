481222393f23d5918b20b5a7d45a535552beda18
IlyaOlokin <i.olokin@mail.ru>
2024-02-16 19:15:47 +0500

    dash

--- Underdark/Assets/Scripts/PushZone.cs
+++ Underdark/Assets/Scripts/PushZone.cs
@@ -3,21 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PushZone : MonoBehaviour, IAttacker
+public class PushZone : MonoBehaviour, IAttackerTarget
 {
     public Transform Transform => transform;
-    
-    [SerializeField] private LayerMask mask;
-    [SerializeField] private PushInfo pushInfo;
-    
+
+    private Unit caster;
+    private DamageInfo damageInfo;
+    private List<DebuffInfo> debuffInfos;
+    private int abilityLevel;
+
+    public void Init(Unit caster, DamageInfo damageInfo, List<DebuffInfo> debuffInfos, int abilityLevel)
+    {
+        this.caster = caster;
+        this.damageInfo = damageInfo;
+        this.debuffInfos = debuffInfos;
+        this.abilityLevel = abilityLevel;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (mask == (mask | (1 << other.gameObject.layer)))
+        if (caster.AttackMask == (caster.AttackMask | (1 << other.gameObject.layer)))
         {
             if (other.TryGetComponent(out Unit unit))
             {
-                pushInfo.Execute(this, unit, null);
+                Attack(unit);
+                foreach (var debuffInfo in debuffInfos)
+                {
+                    debuffInfo.Execute(this, unit, caster);
+                }
             }
         }
+    }
+    
+    public void Attack(IDamageable damageable)
+    {
+        if (abilityLevel <= 1) return;
+        damageable.TakeDamage(caster, this, damageInfo);
     }
 }
