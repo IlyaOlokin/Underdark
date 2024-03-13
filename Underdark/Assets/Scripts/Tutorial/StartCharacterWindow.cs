373d8bb962775bb78c230a5b9ad8b8387fd29302
IlyaOlokin <i.olokin@mail.ru>
2024-03-13 12:03:57 +0500

    block input on character start

--- Underdark/Assets/Scripts/Tutorial/StartCharacterWindow.cs
+++ Underdark/Assets/Scripts/Tutorial/StartCharacterWindow.cs
@@ -9,17 +9,20 @@ public class StartCharacterWindow : MonoBehaviour
     [SerializeField] private CharacterWindowUI characterWindowUI;
     [SerializeField] private GameObject blackOut;
     private Player player;
+    private IInput input;
     
     [Inject]
-    private void Construct(Player player)
+    private void Construct(Player player, IInput input)
     {
         this.player = player;
         characterWindowUI.Init(player);
         characterWindowUI.blackOut = blackOut;
+        this.input = input;
     }
 
     private void Start()
     {
+        input.IsEnabled = false;
         TryCloseWindow();
     }
 
@@ -30,8 +33,10 @@ public class StartCharacterWindow : MonoBehaviour
     
     private void TryCloseWindow()
     {
-        if (characterWindowUI.Stats.FreePoints == 0)
-            Destroy(gameObject);
+        if (characterWindowUI.Stats.FreePoints != 0) return;
+        
+        input.IsEnabled = true;
+        Destroy(gameObject);
     }
     
     private void OnDisable()
